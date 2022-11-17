9be0028692461e9af1a4966f982449d24920f800
J.Dieguez.Vila@student.rug.nl <s5091217@peregrine.hpc.rug.nl>
2022-11-17 02:02:56 +0100

    arreglar el merge

--- episodic-memory-main/MQ/Models/XGPN.py
+++ episodic-memory-main/MQ/Models/XGPN.py
@@ -46,8 +46,8 @@ class XGPN(nn.Module):
 
     def _make_levels_enc(self, opt, in_channels, out_channels, stride = 2):
         if self.use_ViT:
-            return ViT(0, 0, 512, 2048,
-                 8, 1, 0.1, 0.1, lr=0.1,
+            print("---- Creamos un ViT ----")
+            return ViT(0, 0, 512, 2048, 8, 1, 0.1, 0.1, lr=0.1,
                  use_bias=False, num_classes=10, usewandb=False, optimizer_name="SGD")
         elif self.use_xGPN:
             return xGN(opt, in_channels=in_channels, out_channels=out_channels, stride = stride)
@@ -108,3 +108,4 @@ class XGPN(nn.Module):
         feats_dec = self._decoder(feats_enc)
 
         return feats_enc, feats_dec
+
